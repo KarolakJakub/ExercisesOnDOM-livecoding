60845cc460ae41a41c31fff0a428450a683475ae
Jakub K <karolakjakub90@gmail.com>
2019-06-07 11:53:57 +0200

    adding elements via JS

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let display = document.getElementById("display")
+let display = document.getElementById('display')
 display.disabled = true
 
-let operators = document.getElementsByClassName("operator")
+let operators = document.getElementsByClassName('operator')
 operators = Array.from(operators)
 
-let buttons = document.getElementsByTagName("button")
+let buttons = document.getElementsByTagName('button')
 let numbers = Array.from(buttons).filter((button) => {
     let number = Number(button.textContent)
     if (isNaN(number)) {
@@ -15,3 +15,31 @@ let numbers = Array.from(buttons).filter((button) => {
 
 })
 console.log(numbers)
+
+let divs = Array.from(document.firstElementChild.children[1].children).filter(element => element.tagName === 'DIV')
+
+
+function addElement(element, text, target){
+
+    let htmlElement = document.createElement(element)
+    htmlElement.innerText = text
+    target.after(htmlElement)
+
+}
+
+addElement('button', 'AC', display)
+
+let dot = document.createElement('button')
+dot.innerText = "."
+
+let space = document.createElement('text')
+space.innerText = " "
+
+display = document.lastElementChild.lastElementChild.children[4].children[1]
+
+// insertBefore(dot, display)
+let lastRowButtons = document.lastElementChild.lastElementChild.children[4]
+let zeroButton = lastRowButtons.firstElementChild
+
+lastRowButtons.insertBefore(space, display)
+lastRowButtons.insertBefore(dot, display)
